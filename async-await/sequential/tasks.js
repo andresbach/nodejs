6d5fca8b78a581d7002c003621bc365bbfc04566
Andres Bachfischer <38696580+andresbach@users.noreply.github.com>
2018-12-19 12:41:19 -0300

    Upgraded

--- async-await/sequential/tasks.js
+++ async-await/sequential/tasks.js
@@ -9,13 +9,22 @@ module.exports = {
 
 	// esta tarea tiene un delay de 4s
 	async taskOne() {
-		await sleep(4000);
-		return 'ONE VALUE';
+		try {
+			throw new Error('SOME PROBLEM');
+			await sleep(4000);
+			return 'ONE VALUE';
+		} catch(e) {
+				console.log(e);
+		}
 	},
 
 	// esta tarea tiene un delay de 2s
 	async taskTwo() {
-		await sleep(2000);
-		return 'TWO VALUE';
+		try {
+			await sleep(2000);
+			return 'TWO VALUE';
+		} catch(e) {
+			console.log(e);
+		}
 	}
 };

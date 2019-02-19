87736cc1be6f2695676bb968e54d9e193a333603
Andres Bachfischer <38696580+andresbach@users.noreply.github.com>
2019-02-19 16:59:14 -0300

    Testing queries inside the request with express.

--- payload-request/index.js
+++ payload-request/index.js
@@ -4,9 +4,15 @@ var app = express();
 
 app.use(express.json());
 
-app.post('/', function(request, response){
-	console.log(request.body);      // input
-	response.send(request.body);    // output
+app.post('/', function(req, res){
+	console.log(req.body);      // input
+	res.send(req.body);    // output
 });
+
+app.post('/send/:message', function(req, res) {
+	let message = req.params.message;
+	let page = req.query.page;
+	res.send(message + ' ' + page);
+})
 
 app.listen(3000);
